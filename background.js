998d119e1886dd4618c35d2d093580729c4916c2
KentoAdachi <k.adachi0312@gmail.com>
2024-06-02 16:01:49 +0900

    chore: Refactor background.js for improved readability and maintainability

--- background.js
+++ background.js
@@ -1,25 +1,38 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "checkMemo") {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      if (tabs[0]) {
-        const url = tabs[0].url;
-        chrome.storage.local.get(url, function (data) {
-          if (data[url]) {
-            chrome.action.setIcon({
-              path: "icon.png",
-              tabId: tabs[0].id,
-            });
-            sendResponse({ result: data[url] });
-          } else {
-            chrome.action.setIcon({
-              path: "icon-gray.png",
-              tabId: tabs[0].id,
-            });
-            sendResponse({ result: "" });
-          }
-        });
-      }
-    });
-    return true;
+    handleCheckMemo(sendResponse);
+    return true; // Ensure asynchronous response
   }
 });
+
+function handleCheckMemo(sendResponse) {
+  getActiveTab().then((tab) => {
+    if (tab) {
+      checkMemoForTab(tab, sendResponse);
+    }
+  });
+}
+
+function getActiveTab() {
+  return new Promise((resolve) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      resolve(tabs[0]);
+    });
+  });
+}
+
+function checkMemoForTab(tab, sendResponse) {
+  const url = tab.url;
+  chrome.storage.local.get(url, function (data) {
+    const iconPath = data[url] ? "icon.png" : "icon-gray.png";
+    updateIcon(tab.id, iconPath);
+    sendResponse({ result: data[url] || "" });
+  });
+}
+
+function updateIcon(tabId, path) {
+  chrome.action.setIcon({
+    path: path,
+    tabId: tabId,
+  });
+}
